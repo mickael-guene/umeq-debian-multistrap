597946ca7f0d90eec7d73f9ca7fc82513dd04bca
Mickael Guene <mickael.guene@st.com>
2016-07-26 12:38:38 +0200

    try again

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,20 +9,17 @@ SCRIPTDIR=`(cd $SCRIPTDIR ; pwd)`
 ARCHS="arm64 armhf"
 VERSIONS="jessie testing"
 
-#add deploy key
-cd ${SCRIPTDIR}
-eval `ssh-agent -s`
+#push rootfs
 for arch in ${ARCHS}; do
+    # deploy key
     set +x
     openssl aes-256-cbc -K $encrypted_f1bb190f457f_key -iv $encrypted_f1bb190f457f_iv -in deploy_key_${arch}.enc -out deploy_key -d
     set -x
     chmod 600 deploy_key
+    eval `ssh-agent -s`
     ssh-add deploy_key
     rm deploy_key
-done
-
-#push rootfs
-for arch in ${ARCHS}; do
+    # deploy commit
     for version in ${VERSIONS}; do
         . ${SCRIPTDIR}/deploy_common.sh ${arch} ${version} ${SCRIPTDIR}/../${arch}-debian-${version}.tgz
     done
