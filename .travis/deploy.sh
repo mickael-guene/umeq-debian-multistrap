15d32daf9d6675dab0226d0e59b7dec272a9450e
Mickael Guene <mickael.guene@st.com>
2016-07-26 12:23:01 +0200

    should be ok

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,9 +12,9 @@ VERSIONS="jessie testing"
 #add deploy key
 cd ${SCRIPTDIR}
 for arch in ${ARCHS}; do
-    #set +x
+    set +x
     openssl aes-256-cbc -K $encrypted_f1bb190f457f_key -iv $encrypted_f1bb190f457f_iv -in deploy_key_${arch}.enc -out deploy_key -d
-    #set -x
+    set -x
     chmod 600 deploy_key
     eval `ssh-agent -s`
     ssh-add deploy_key
