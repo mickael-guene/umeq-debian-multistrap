a95048fa102e1dfa82acbe8643fefa6281e7c062
Mickael Guene <mickael.guene@st.com>
2016-07-26 09:48:45 +0200

    change deploy key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,12 +8,14 @@ SCRIPTDIR=`(cd $SCRIPTDIR ; pwd)`
 WDIR=`mktemp -d` && trap "rm -Rf $WDIR" EXIT
 
 #add deploy key
+set +x
 cd ${SCRIPTDIR}
 openssl aes-256-cbc -K $encrypted_f1bb190f457f_key -iv $encrypted_f1bb190f457f_iv -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
 rm deploy_key
+set -x
 
 #switch to tmp dir and setup new git repo
 cd ${WDIR}
@@ -27,6 +29,7 @@ git pull --depth=1 origin jessie
 git checkout -b jessie
 
 #create commit
+cp ${SCRIPTDIR}/arm64-debian-jessie.tgz .
 cat << EOF > Dockerfile
 FROM scratch
 MAINTAINER ${GIT_USER_NAME} <${GIT_USER_EMAIL}>
